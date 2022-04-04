a3bbef59aa57dc6dddceb8f6d8a677f45bc774a3
Stephen Gutekanst <stephen@sourcegraph.com>
2022-04-04 11:50:52 -0700

    share panels logic between pages
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- src/Pages/S/Chat/Zulip.elm
+++ src/Pages/S/Chat/Zulip.elm
@@ -8,6 +8,7 @@ import Element.Font as Font
 import Element.Region as Region
 import Layout
 import Page
+import Panels
 import Request exposing (Request)
 import Shared
 import Url.Parser exposing (..)
@@ -15,12 +16,12 @@ import View exposing (View)
 
 
 type alias Model =
-    { panels : Array Compute.Model
+    { panels : Panels.Model
     }
 
 
 type Msg
-    = PanelMsg Int Compute.Msg
+    = PanelsMsg Panels.Msg
 
 
 page : Shared.Model -> Request -> Page.With Model Msg
@@ -64,47 +65,21 @@ init shared =
             , selectedTab = Just "number"
             }
 
-        ( panels, cmds ) =
-            List.unzip (initPanels PanelMsg shared [ panel0, panel1 ])
+        ( panelsModel, panelsCmd ) =
+            Panels.init PanelsMsg shared [ panel0, panel1 ]
     in
-    ( { panels = Array.fromList panels }, Cmd.batch cmds )
-
-
-initPanels : (Int -> Compute.Msg -> msg) -> Shared.Model -> List ComputeBackend.ComputeInput -> List ( Compute.Model, Cmd msg )
-initPanels panelMsg shared panels =
-    List.indexedMap
-        (\index panelInput ->
-            let
-                flags =
-                    shared.flags
-
-                mergedFlags =
-                    { flags | computeInput = Just panelInput }
-
-                ( subModel, subCmd ) =
-                    Compute.init { shared | flags = mergedFlags }
-            in
-            ( subModel, Cmd.map (panelMsg index) subCmd )
-        )
-        panels
+    ( { panels = panelsModel }, panelsCmd )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        PanelMsg index subMsg ->
-            case Array.get index model.panels of
-                Just panel ->
-                    let
-                        ( subModel, subCmd ) =
-                            Compute.update subMsg panel
-                    in
-                    ( { model | panels = Array.set index subModel model.panels }
-                    , Cmd.map (PanelMsg index) subCmd
-                    )
-
-                Nothing ->
-                    ( model, Cmd.none )
+        PanelsMsg subMsg ->
+            let
+                ( subModel, subCmd ) =
+                    Panels.update PanelsMsg subMsg model.panels
+            in
+            ( { model | panels = subModel }, subCmd )
 
 
 view : Model -> View Msg
@@ -115,11 +90,11 @@ view model =
             [ E.column [ E.centerX, E.paddingXY 0 64 ]
                 [ E.el [ Region.heading 1, Font.size 24 ] (E.text "Zulip chat stats from 2m+ OSS repositories")
                 , E.el [ Region.heading 2, Font.size 20, E.paddingEach { top = 64, right = 0, bottom = 0, left = 0 } ] (E.text "Top 10 most-linked Zulip chat groups")
-                , renderPanel PanelMsg model.panels 0
+                , Panels.render PanelsMsg model.panels 0
                 , E.el [ Region.heading 2, Font.size 20, E.paddingEach { top = 64, right = 0, bottom = 0, left = 0 } ]
                     (E.row []
                         [ E.text "Unique Zulip chat rooms across 2m+ repos: "
-                        , renderPanel PanelMsg model.panels 1
+                        , Panels.render PanelsMsg model.panels 1
                         ]
                     )
                 ]
@@ -127,26 +102,6 @@ view model =
     }
 
 
-renderPanel : (Int -> Compute.Msg -> msg) -> Array Compute.Model -> Int -> E.Element msg
-renderPanel panelMsg panels index =
-    case Array.get index panels of
-        Just panel ->
-            E.map (panelMsg index) (Compute.view panel)
-
-        Nothing ->
-            E.none
-
-
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.batch
-        (Array.toList
-            (Array.indexedMap
-                (\index panel ->
-                    Sub.map
-                        (PanelMsg index)
-                        (Compute.subscriptions panel)
-                )
-                model.panels
-            )
-        )
+    Panels.subscriptions PanelsMsg model.panels
