1f3529efc83df2b21af17365140126c21989e14a
Stephen Gutekanst <stephen@sourcegraph.com>
2022-04-05 18:53:08 -0700

    better wording everywhere
    Signed-off-by: Stephen Gutekanst <stephen@sourcegraph.com>

--- src/Pages/Explorer.elm
+++ src/Pages/Explorer.elm
@@ -59,37 +59,15 @@ view model =
             [ E.column [ E.centerX, E.paddingXY 0 64 ]
                 [ E.el [ Region.heading 1, Font.size 24 ] (E.text "Compute data explorer")
                 , E.el [ E.paddingXY 0 32, E.width E.fill ] (E.map WordCloudMsg (Compute.view model.wordCloud))
-                , E.el [ Region.heading 2, Font.size 20, E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 } ] (E.text "About")
-                , about
-                , E.el [ Region.heading 2, Font.size 20, E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 } ] (E.text "Who's behind this?")
-                , about
+                , E.el [ Region.heading 2, Font.size 20, E.paddingEach { top = 64, right = 0, bottom = 0, left = 0 } ] (E.text "How do I use this?")
+                , E.paragraph [ E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 }, E.width (E.fill |> E.maximum 800) ]
+                    [ E.text "Documentation is lacking right now, sorry! We're working on it. For now I suggest you check out "
+                    , E.link [] { url = "https://twitter.com/rvtond/status/1509677515761094659", label = E.text "this Twitter thread" }
+                    , E.text " which details some of the capabilities here."
+                    ]
                 ]
             ]
     }
-
-
-about : E.Element msg
-about =
-    E.column []
-        [ E.paragraph [ E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 }, E.width (E.fill |> E.maximum 800) ]
-            [ E.text "codestat.dev leverages an "
-            , E.link [] { url = "https://twitter.com/rvtond/status/1509677515761094659", label = E.text "experimental Sourcegraph compute API" }
-            , E.text " to perform regex searches over the code in 2m+ open source repositories"
-            , E.text ", and then perform computation over the result set. "
-            , E.link [] { url = "https://elm-lang.org", label = E.text "The Elm language" }
-            , E.text " is used to visualize the results, and render the data explorer above."
-            ]
-        , E.paragraph [ E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 }, E.width (E.fill |> E.maximum 800) ]
-            [ E.text "You can learn more about how powerful this all is "
-            , E.link [] { url = "https://twitter.com/rvtond/status/1509677515761094659", label = E.text "via this thread" }
-            , E.text " from the creator of Compute at Sourcegraph."
-            ]
-        , E.paragraph [ E.paddingXY 0 32, E.width (E.fill |> E.maximum 800) ]
-            [ E.text "All this stuff is pretty early stages! Feel free to tweet us "
-            , E.link [] { url = "https://twitter.com/codestat_dev", label = E.text "@codestat_dev" }
-            , E.text " for help/requests!"
-            ]
-        ]
 
 
 subscriptions : Model -> Sub Msg
--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -78,37 +78,58 @@ view model =
                     , E.text " computation in real time, as thousands of results stream in to your browser!"
                     ]
                 , E.paragraph [ E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 }, E.width (E.fill |> E.maximum 800) ]
-                    [ E.text "For example-we can search the top 100 Go repositories and find the most popular interface type name:"
+                    [ E.text "Ever wondered what most popular `interface` type name is in the top 100 Go repositories? We can answer that:"
                     ]
                 , E.el [ E.paddingEach { top = 32, right = 0, bottom = 50, left = 0 }, E.width E.fill ] (Panels.render PanelsMsg model.panels 0)
-                , E.paragraph [ E.paddingXY 0 32, E.width (E.fill |> E.maximum 800) ]
-                    [ E.text "Once results stop loading, you'll see Logger is the most popular name found in ~33 of the top Go repositories!"
+                , E.paragraph [ E.paddingEach { top = 48, right = 0, bottom = 0, left = 0 }, E.width (E.fill |> E.maximum 800) ]
+                    [ E.text "Once the results finish loading, you'll see that \"Logger\" is the most popular Go interface name found and is found in 33 of the top 100 Go repositories!"
                     ]
-                , E.el [ Region.heading 2, Font.size 20, E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 } ] (E.text "Recent project stats")
-                , E.column [ E.paddingEach { top = 16, right = 0, bottom = 32, left = 0 } ]
-                    [ E.row []
+                , E.el [ Region.heading 2, Font.size 20, E.paddingEach { top = 48, right = 0, bottom = 0, left = 0 } ] (E.text "🔥 The cool stuff (LOOK HERE) 🔥")
+                , E.column [ E.paddingEach { top = 16, right = 0, bottom = 16, left = 0 } ]
+                    [ E.row [ E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 } ]
                         [ E.text "• "
-                        , E.link [ Region.heading 1, E.paddingXY 0 8 ] { url = "/s/zulip", label = E.text "Zulip" }
-                        , E.text " - chat software for distributed teams"
+                        , E.link [ Region.heading 1, E.paddingXY 0 8, Font.size 18 ] { url = "/s/zulip", label = E.text "Zulip" }
+                        , E.text " - stats about the chat software for distributed teams"
                         ]
                     , E.row []
-                        [ E.text "• more projects coming soon.."
+                        [ E.text "• "
+                        , E.el [ Font.size 12 ] (E.text "more coming soon..")
+                        ]
+                    , E.paragraph [ E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 }, E.width (E.fill |> E.maximum 800) ]
+                        [ E.text "We create dedicated pages for OSS projects & random one-off stats you're curious about. Just tweet "
+                        , E.link [] { url = "https://twitter.com/codestat_dev", label = E.text "@codestat_dev" }
                         ]
                     ]
-                , E.el [ Region.heading 2, Font.size 20, E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 } ] (E.text "How it works")
+                , E.el [ Region.heading 2, Font.size 20, E.paddingEach { top = 48, right = 0, bottom = 0, left = 0 } ] (E.text "Tech details")
                 , E.paragraph [ E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 }, E.width (E.fill |> E.maximum 800) ]
-                    [ E.text "Behind the scenes we leverage an experimental "
-                    , E.link [] { url = "https://twitter.com/rvtond/status/1509677515761094659", label = E.text "Sourcegraph compute API" }
-                    , E.text " to perform computation over thousands to millions of search results from Sourcegraph code search"
-                    , E.text " (you can explore yourself using the "
-                    , E.link [] { url = "/explorer", label = E.text "compute data explorer" }
-                    , E.text ".) We then use the "
-                    , E.link [] { url = "https://elm-lang.org", label = E.text "Elm language" }
-                    , E.text " to visualize the results."
+                    [ E.text "This leverage an experimental "
+                    , E.link [] { url = "https://twitter.com/rvtond/status/1509677515761094659", label = E.text "Sourcegraph search compute API" }
+                    , E.text " which, behind the scenes, searches over a very large trigram index (roughly the top 2 million GitHub repositories by stars) so that we can do regexp matching over *all* that code, stream every match back to the browser using the EventSource API, and finally visualize them using the "
+                    , E.link [] { url = "https://elm-lang.org", label = E.text "Elm language." }
                     ]
-                , E.paragraph [ E.paddingXY 0 32, E.width (E.fill |> E.maximum 800) ]
-                    [ E.text "We create dashboards for interesting stats/projects and post them to our Twitter "
-                    , E.link [] { url = "https://twitter.com/codestat_dev", label = E.text "@codestat_dev" }
+                , E.paragraph [ E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 }, E.width (E.fill |> E.maximum 800) ]
+                    [ E.text " You can even "
+                    , E.link [] { url = "/explorer", label = E.text "explore the data" }
+                    , E.text " using your own queries!"
+                    ]
+                , E.el [ Region.heading 2, Font.size 20, E.paddingEach { top = 48, right = 0, bottom = 0, left = 0 } ] (E.text "History")
+                , E.paragraph [ E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 }, E.width (E.fill |> E.maximum 800) ]
+                    [ E.link [] { url = "https://twitter.com/slimsag", label = E.text "Stephen Gutekanst" }
+                    , E.text " here! I've been working at "
+                    , E.link [] { url = "https://sourcegraph.com", label = E.text "Sourcegraph" }
+                    , E.text " a little over 7 years, and a coworker of mine ("
+                    , E.link [] { url = "https://twitter.com/rvtond", label = E.text "Rijnard Van Tonder" }
+                    , E.text ", researcher on the Search Core team) has been tirelessly building out this 'search compute' engine for the past few years as a sort of one-man passion project. I remember the day he joined he was talking about building this, and has since pulled in a few other passionate engineers!"
+                    ]
+                , E.paragraph [ E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 }, E.width (E.fill |> E.maximum 800) ]
+                    [ E.text " Recently, the backend API began to really come together. At the same time, I kept hearing great things about the Elm language, and it turned out he was using it to prototype! "
+                    , E.text " The more I played around with his work, the more powerful I thought it was and I wanted to find a nice way to share that power with others "
+                    ]
+                , E.paragraph [ E.paddingEach { top = 32, right = 0, bottom = 0, left = 0 }, E.width (E.fill |> E.maximum 800) ]
+                    [ E.text "Conveniently, my manager had just given me a week to try out a crazy-idea *and* I had another full week for a hackathon - two weeks! - so I took the opportunity to learn Elm (which was hard, but worth it) and pester my coworker for all his knowledge. codestat.dev was born! "
+                    ]
+                , E.paragraph [ E.paddingEach { top = 32, right = 0, bottom = 64, left = 0 }, E.width (E.fill |> E.maximum 800) ]
+                    [ E.text "This isn't an officially supported Sourcegraph project for now, all very experimental! If you like it, let us know! "
                     ]
                 ]
             ]
